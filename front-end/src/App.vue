d22696d584c5a883afec2e40ed50e842485a6470
kevinKaikai <2213230314@qq.com>
2020-03-15 12:12:58 +0800

    项目国际化搭建完成

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>{{ $t('message.hello') }}</el-header>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
-
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      msg: "longkai",
+    };
+  }
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
+  line-height: 60px;
 }
 
-#nav {
-  padding: 30px;
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+import i18n from './i18n/i18n'
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    i18n,
+    render: h => h(App)
 }).$mount('#app')
